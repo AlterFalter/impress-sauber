69f2b08ab3a8f037e943b6c3e667432f2260ae3d
Yannis Krämer <yannkra21@gmail.com>
2024-04-15 21:48:59 +0200

    Add SauberData project and CursorPositionData.cs.

---
+++ src/SauberData/CursorPositionData.cs
@@ -0,0 +1,21 @@
+﻿namespace SauberData
+{
+    public class CursorPositionData
+    {
+        public int CursorPositionX { get; set; }
+        public int CursorPositionY { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public CursorPositionData(int x, int y)
+            : this(x, y, DateTime.Now)
+        {
+        }
+
+        public CursorPositionData(int x, int y, DateTime dateTime)
+        {
+            this.CursorPositionX = x;
+            this.CursorPositionY = y;
+            this.DateTime = dateTime;
+        }
+    }
+}
